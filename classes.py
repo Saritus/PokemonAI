ddec80867dc0ef696e2275d3fdfaa164309ad7c8
Sebastian Mischke <SebastianMischke@gmx.de>
2017-07-25 23:41:09 +0200

    Save types

--- classes.py
+++ classes.py
@@ -4,6 +4,10 @@ from data import get_pokemon
 class Pokemon:
     def __init__(self, index):
         self.data = get_pokemon(index)
+        self.types = [
+            self.data["Type1"],
+            self.data["Type2"]
+        ]
 
 
 def main():
