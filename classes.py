3a3346b3bba30e17736018b0b5d21c99c41250db
Sebastian Mischke <SebastianMischke@gmx.de>
2017-07-25 23:45:11 +0200

    Start stat calculation

--- classes.py
+++ classes.py
@@ -1,15 +1,20 @@
 from data import get_pokemon
+from math import floor
 
 
 class Pokemon:
-    def __init__(self, index):
+    def __init__(self, index, level=5):
+        self.level = level
         self.data = get_pokemon(index)
         self.types = [
             self.data["Type1"],
             self.data["Type2"]
         ]
+        B = 1  # B (Base Stat)
+        I = 1  # I (Individual Value)
+        L = self.level  # L (Level)
         self.stats = {
-            "Hit Points": 0,
+            "HP": floor((2 * B + I) * L / 100 + 5),
             "Attack": 0,
             "Defense": 0,
             "Special": 0,
