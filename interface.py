2d5f860cfc5109e601298ec54d9ef845aaf23c9d
Sebastian Mischke <SebastianMischke@gmx.de>
2017-07-30 07:24:49 +0200

    Improve layout

--- interface.py
+++ interface.py
@@ -32,29 +32,32 @@ class App:
         opponent = Frame(master)
         opponent.grid(row=0)
 
-        self.o_infos = Label(opponent, text="Opponent")
-        self.o_infos.pack(side=RIGHT)
+        self.o_infos = Label(opponent, text="Opponent", width=30)
+        self.o_infos.pack(side=LEFT)
         self.o_canvas = Canvas(opponent)
 
-        self.o_img = Image.open("Sprites/110.PNG")
+        self.o_img = Image.open("Sprites/110.PNG").resize((100, 100))
         self.o_photo = ImageTk.PhotoImage(self.o_img)
-        self.o_canvas.create_image(20, 20, image=self.o_photo)
-        self.o_canvas.pack(side=LEFT)
+        self.o_canvas.config(width=self.o_img.width, height=self.o_img.height)
+        self.o_canvas.create_image(self.o_img.width / 2, self.o_img.height / 2, image=self.o_photo)
+        self.o_canvas.pack(side=RIGHT)
 
         # Current
         current = Frame(master)
         current.grid(row=1)
 
-        self.c_infos = Label(current, text="Current")
-        self.c_infos.pack(side=LEFT)
+        self.c_infos = Label(current, text="Current", width=30)
+        self.c_infos.pack(side=RIGHT)
         self.c_canvas = Canvas(current)
 
-        self.c_img = Image.open("Sprites/100.PNG")
+        self.c_img = Image.open("Sprites/110.PNG").resize((100, 100))
         self.c_photo = ImageTk.PhotoImage(self.c_img)
-        self.c_canvas.create_image(20, 20, image=self.c_photo)
-        self.c_canvas.pack(side=RIGHT)
+        self.c_canvas.config(width=self.c_img.width, height=self.c_img.height)
+        self.c_canvas.create_image(self.c_img.width / 2, self.c_img.height / 2, image=self.c_photo)
+        self.c_canvas.pack(side=LEFT)
 
     def change_image(self):
+        self.c_infos.configure(text="Anderer text")
         return
 
 
